843044f524ff9d27f5594e830995f52410a8343e
JacopoNa <j.nardelli@virgilio.it>
2022-07-25 17:45:50 +0200

    php question & answer

--- index.php
+++ index.php
@@ -4,6 +4,31 @@ https://policies.google.com/faq
 Ci sono diverse domande con relative risposte.
 Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
 
+
+<?php 
+$data = [
+    'request' => [
+        'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+        'answer' => "La recente <a class='highlighted' href='#'> decisione della Corte di giustizia dell'Unione europea </a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.<br><br><br>
+                     Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.<br><br><br>
+                     Per presentare una richiesta di rimozione, compila questo <a class='highlighted' href='#'>modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.<br><br><br>
+                     "
+    ],
+    [
+        'question' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        'answer' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.<br><br><br>
+        Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.<br><br><br>
+        Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, <a class='highlighted' href='#'>nel Centro Google per la sicurezza online</a>.<br><br><br>"
+    ],
+    [
+        'question' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
+        'answer' => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a class='highlighted' href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, <a class='highlighted' href='#'>fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a class='highlighted' href='#'>visitare la nostra pagina di assistenza per avere ulteriori informazioni</a>."
+    ] 
+]
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,6 +42,7 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <!-- header -->
     <header>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png" alt="logo">
         <span>Privacy e Termini</span>
@@ -29,5 +55,16 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
             <li class="selected"><a href="#">Domande frequenti</a></li>
         </ul>
     </header>
+
+    <main>
+        <div class="container">
+            <?php foreach ($data as $key => $text) { ?>
+                <h2><?php echo $text['question']; ?></h2>
+                <p><?php echo $text['answer'];?></p>
+                
+            <?php } ?>
+
+        </div>
+    </main>
 </body>
 </html>
